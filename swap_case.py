335c2d7267f67c0ae4c18437697a894c7184dc9a
Ayisibea <154318314+Ayisibea@users.noreply.github.com>
2025-01-30 20:16:01 +0000

    Add files via upload

---
+++ swap_case.py
@@ -0,0 +1,18 @@
+def swap_case(s):
+    new_s= ''
+    for i in s:
+        if i.islower():
+            new_s+=i.capitalize()
+        elif i.isupper():
+            new_s+=i.lower()
+        else:
+            new_s+= i
+    return new_s    
+            
+            
+            
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
