298ca791bfe7ae0b3335c57fba6ba9488dff89ab
Ayisibea <154318314+Ayisibea@users.noreply.github.com>
2025-02-06 20:43:36 +0000

    Add files via upload

---
+++ nested_lists.py
@@ -0,0 +1,20 @@
+if __name__ == '__main__':
+    student = []
+    
+    for _ in range(int(input())):
+        name = input()
+        score = float(input())
+        n_student = [score,name]
+        student.append(n_student)
+    student.sort()
+    low = student[0][0]
+    # count = 0
+    # while count<2:
+    for i in range(len(student)):
+        if student[i][0] > low:
+            low = student[i][0]
+            break 
+    for i in range(len(student)):
+        if student[i][0] == low:
+            print(student[i][1])
+    
